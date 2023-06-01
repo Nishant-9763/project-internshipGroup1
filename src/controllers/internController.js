87f09bd89d02410dd678f00fa989e5f0761ec3f0
Nishant-9763 <119596151+Nishant-9763@users.noreply.github.com>
2023-06-01 17:17:40 +0530

    Update internController.js

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -25,7 +25,7 @@ const createIntern = async function(req, res) {
 
         if (!collegeName) return res.status(400).send({status: false, msg: 'college name is required'});
         if (!validCollegeName(collegeName)) return res.status(400).send({status: false, msg: 'please enter a valid college name'});
-        const college = await collegeModel.findOne({name: collegeName});
+        const college = await collegeModel.findOne({name: collegeName,isDeleted:false});
         if (!college) return res.status(400).send({status: false, msg: 'collge does not exits with this name'});
 
         data.collegeId = college._id;
